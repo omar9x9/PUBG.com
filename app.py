bc28fd3704a04347c99fa47732b41513a5b0a487
omar9x9 <mrm449094@gmail.com>
2026-08-15 17:32:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if not TELEGRAM_TOKEN:
     raise ValueError("TELEGRAM_TOKEN not set")
 
 # ========== Flask ==========
-app = Flask (_name, template_folder='.')
+app = Flask(__name__, template_folder='.')
 # ========== قاعدة البيانات ==========
 DB_PATH = "data.db"
 
